b75c1d01b09a4bd7b56e877d3d7922b4b4aadbd3
Tjaž Eržen <erzen.tjaz@gmail.com>
2019-08-12 21:11:39 +0200

    Deleting a test file

--- testna.py
+++
@@ -1,7 +0,0 @@
-sentences_file = open("data.txt", "r")
-sentences = sentences_file.readlines()
-for number, sentence in enumerate(sentences, 1):
-    print(number, sentence)
-
-
-sentences_file.close()
